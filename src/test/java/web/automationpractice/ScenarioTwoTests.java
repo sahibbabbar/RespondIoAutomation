1b4cbb63102aa2945ec432664e8f1814b31dfc58
TPG Apprentice <sahib.babbar@modmed.com>
2022-09-21 02:10:40 -0400

    Web Automation Test Class - Scenario #2

---
+++ src/test/java/web/automationpractice/ScenarioTwoTests.java
@@ -0,0 +1,83 @@
+package web.automationpractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import platform.web.constants.Urls;
+import platform.web.screens.FilterPanel;
+import platform.web.screens.HomeScreen;
+import platform.web.screens.ProductScreen;
+import platform.web.screens.WomenCategoryScreen;
+import web.CommonWebTest;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Scenario 2: Users are able to filter search results under `Women` category by `Color` and `Category`
+ */
+public class ScenarioTwoTests extends CommonWebTest {
+    private int RANDOM_CATEGORY_INDEX;
+    private int RANDOM_COLOR_INDEX;
+
+    @Test
+    public void testFilterResultsUnderWomenByColorAndCategory() {
+        // Navigate to the homepage
+        driver.get(Urls.URL_HOME_PAGE);
+        driver.findElement(By.xpath(HomeScreen.TOP_MENU_WOMEN_LINK)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(WomenCategoryScreen.WOMEN_CATEGORY_TITLE)));
+        Assert.assertEquals(driver.findElement(By.xpath(ProductScreen.BREADCRUMB)).getText().trim(), "WOMEN", "Verify the breadcrumb displaying the correct value.");
+
+        // Filter Categories and verify the breadcrumb content
+        filterCategories();
+        scroll(By.xpath(HomeScreen.TOP_MENU_WOMEN_LINK));
+        String categoryName = driver.findElement(By.xpath(FilterPanel.CATEGORIES + "[" + RANDOM_CATEGORY_INDEX + "]/label//a")).getText().split(" ")[0].toLowerCase();
+        Assert.assertEquals(driver.findElement(By.xpath(ProductScreen.BREADCRUMB)).getText(), "WOMEN > CATEGORIES " + categoryName.toUpperCase(), "Verify the breadcrumb displaying the correct value.");
+
+        // Filter Colors and verify the breadcrumb content
+        filterColors();
+        scroll(By.xpath(HomeScreen.TOP_MENU_WOMEN_LINK));
+        Integer colorCount = Integer.parseInt(driver.findElement(By.xpath(FilterPanel.COLORS + "[" + RANDOM_COLOR_INDEX + "]/label//span")).getText().replaceAll("\\p{P}", ""));
+        String colorName = driver.findElement(By.xpath(FilterPanel.COLORS + "[" + RANDOM_COLOR_INDEX + "]/label//a")).getText().split(" ")[0].toLowerCase();
+        Assert.assertEquals(driver.findElement(By.xpath(ProductScreen.BREADCRUMB)).getText(), "WOMEN > CATEGORIES " + categoryName.toUpperCase() + " > COLOR " + colorName.toUpperCase(), "Verify the breadcrumb displaying the correct value.");
+
+        Integer categoryCount = Integer.parseInt(driver.findElement(By.xpath(FilterPanel.CATEGORIES + "[" + RANDOM_CATEGORY_INDEX + "]/label//span")).getText().replaceAll("\\p{P}", ""));
+        Assert.assertEquals(categoryCount, colorCount, "Verify the count of both the filters is equal");
+
+        List<WebElement> productElements = driver.findElements(By.xpath(ProductScreen.PRODUCT_CONTAINER));
+        if (productElements.size() != 0) {
+            for (WebElement productElement : productElements) {
+                Assert.assertTrue(productElement.findElement(By.xpath(".//a[contains(@href,'" + colorName + "')]")).isDisplayed(), "Verify the filtered color is same in the listed product");
+            }
+        }
+    }
+
+    /**
+     * Getting the existing list of categories and filtering on random basis.
+     */
+    private void filterCategories() {
+        List<WebElement> categories = driver.findElements(By.xpath(FilterPanel.CATEGORIES));
+        RANDOM_CATEGORY_INDEX = new Random().nextInt(categories.size()) + 1;
+
+        if (categories.size() != 0) {
+            driver.findElement(By.xpath(FilterPanel.CATEGORIES + "[" + RANDOM_CATEGORY_INDEX + "]")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='enabled_filters']//li[1]")));
+        }
+    }
+
+    /**
+     * Getting the existing list of colors and filtering on random basis.
+     */
+    private void filterColors() {
+        scroll(By.xpath(FilterPanel.COLORS_FILTER_HEADING));
+        List<WebElement> colors = driver.findElements(By.xpath(FilterPanel.COLORS));
+
+        if (colors.size() != 0) {
+            RANDOM_COLOR_INDEX = new Random().nextInt(colors.size()) + 1;
+            driver.findElement(By.xpath(FilterPanel.COLORS + "[" + RANDOM_COLOR_INDEX + "]")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='enabled_filters']//li[2]")));
+        }
+    }
+}
